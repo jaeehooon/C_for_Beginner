3478ade22c0c0851a328a699b25bdf7a66d90dce
jaehooon <qlenfr0922@gmail.com>
2021-02-04 09:37:31 +0900

    210204 09:37

---
+++ src/ch05/practice/prac_6.c
@@ -0,0 +1,15 @@
+/**
+ * 연습문제 6번 |
+ */
+#include <stdio.h>
+
+int main() {
+    int a = 100;
+
+    if (a < 200) {
+        printf(" a가 200보다");
+        printf(" 작습니다.\n");
+    }
+    else
+        printf(" a가 200보다 큽니다.");
+}
