9b44ce1c0ca55a476139193f80d3b67f74567bd8
jaehooon <qlenfr0922@gmail.com>
2021-02-12 20:21:29 +0900

    210212 20:21

---
+++ src/ch01/practice/prac_2.c
@@ -0,0 +1,11 @@
+/**
+ * 연습문제 2번
+ */
+#include <stdio.h>
+
+void main(){
+    int a, b;
+    a = 1000;
+    b = 1;
+    printf(" %d \n", a - b);
+}
---
+++ src/ch01/practice/prac_3.c
@@ -0,0 +1,8 @@
+/**
+ * 연습문제 3번
+ */
+#include <stdio.h>
+
+int main() {
+    printf("1\n\n\n");
+}
